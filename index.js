82b3fa93776fa6bc54b9a58da26c7512c3fca961
Rohit Hindwan <74043859+rohithindwan@users.noreply.github.com>
2023-03-07 19:58:47 +0530

    fix heading tags

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ fetch("data.json")
   .then((Response) => {
     document.getElementById(
       "first"
-    ).innerHTML += `<img src="${Response[0].icon}" alt="image" /><h4>${Response[0].category} <span>${Response[0].score} / 100</span> </h4>
+    ).innerHTML += `<img src="${Response[0].icon}" alt="image" /><h2>${Response[0].category} <span>${Response[0].score} / 100</span> </h2>
       `;
     document.getElementById(
       "second"
-    ).innerHTML += `<img src="${Response[1].icon}" alt="image" /><h4>${Response[1].category} <span>${Response[1].score} / 100</span> </h4>
+    ).innerHTML += `<img src="${Response[1].icon}" alt="image" /><h2>${Response[1].category} <span>${Response[1].score} / 100</span> </h2>
       `;
     document.getElementById(
       "third"
-    ).innerHTML += `<img src="${Response[2].icon}" alt="image" /><h4>${Response[2].category} <span>${Response[2].score} / 100</span> </h4>
+    ).innerHTML += `<img src="${Response[2].icon}" alt="image" /><h2>${Response[2].category} <span>${Response[2].score} / 100</span> </h2>
       `;
     document.getElementById(
       "forth"
-    ).innerHTML += `<img src="${Response[3].icon}" alt="image" /><h4>${Response[3].category} <span>${Response[3].score} / 100</span> </h4>
+    ).innerHTML += `<img src="${Response[3].icon}" alt="image" /><h2>${Response[3].category} <span>${Response[3].score} / 100</span> </h2>
       `;
 
     const Average = () => {
